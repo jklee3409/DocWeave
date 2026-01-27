60b88ee6d0e7155a7ef793d39a735546d676a0b2
Jong Kyu Lee <jklee3409@khu.ac.kr>
2026-01-27 16:16:05 +0900

    feat: 회원 기능 Common Layer 구현

---
+++ backend/src/main/java/com/docweave/server/common/constant/TokenConstant.java
@@ -0,0 +1,6 @@
+package com.docweave.server.common.constant;
+
+public class TokenConstant {
+    public static final String REFRESH_TOKEN_PREFIX = "refresh_token:";
+    public static final String BLACKLIST_PREFIX = "blacklist:";
+}
